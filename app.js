508815a239e855b3cabd87bcde7f752227b56712
Pratik Singh <56778773+kitarp29@users.noreply.github.com>
2020-06-18 11:27:03 +0530

    v2

--- app.js
+++ app.js
@@ -1,28 +1,80 @@
 //jshint esversion:6
-const express= require("express");
+require('dotenv').config();
+const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 const app = express();
 
 app.use(express.static("public"));
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-app.get("/",function(req,res){
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
+
+const userSchema = new mongoose.Schema({
+  email: {type:String,required:true},
+  password: String
+});
+
+userSchema.plugin(encrypt,{secret:process.env.SECRET ,encryptedFields:["password"] });
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function(req, res) {
   res.render("home");
 });
 
-app.get("/login",function(req,res){
+app.get("/login", function(req, res) {
   res.render("login");
 });
 
-app.get("/register",function(req,res){
+app.get("/register", function(req, res) {
   res.render("register");
 });
 
-app.listen(3000,function(){
+app.post("/register", function(req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save(function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
+
+app.post("/login", function(req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email: username}, function(err, founduser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (founduser) {
+        if (founduser.password === password) {
+          res.render("secrets");
+        }
+        else{
+           req.flash("error");
+        }
+      }
+    }
+  });
+});
+
+
+app.listen(3000, function() {
   console.log("I'm running BITCH");
 })
