04bb4f5f2d86ca53578c14c9afca05933265dbd8
Pratik Singh <56778773+kitarp29@users.noreply.github.com>
2020-06-20 00:39:24 +0530

    Ho hi gya shyad

--- app.js
+++ app.js
@@ -172,11 +172,12 @@ app.post("/login", function(req, res){
 });
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
 
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000.");
+app.listen(port, function() {
+  console.log("Server started");
 });
