afe9acc252e36f80dfa6c4cedf8181494d28b8cf
Pratik Singh <56778773+kitarp29@users.noreply.github.com>
2020-06-20 08:58:04 +0530

    kaam kr jaa BSDK

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "https://aqueous-ravine-15860.herokuapp.com/auth/google/secrets",
+    callbackURL: "/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
