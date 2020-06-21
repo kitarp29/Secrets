2850f184b62652ee1ae32d4d61e047844435f891
Pratik Singh <56778773+kitarp29@users.noreply.github.com>
2020-06-21 13:06:40 +0530

    DB ma duplicate ho jaa rha tha

--- app.js
+++ app.js
@@ -73,15 +73,13 @@ app.get("/", function(req, res){
   res.render("home");
 });
 
-app.get('/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile']
-  })
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ["profile"] })
 );
 
 app.get("/auth/google/secrets",
-passport.authenticate('google', { failureRedirect: "/login" }),
-function(req, res) {
+  passport.authenticate('google', { failureRedirect: "/login" }),
+  function(req, res) {
     // Successful authentication, redirect to secrets.
     res.redirect("/secrets");
   });
